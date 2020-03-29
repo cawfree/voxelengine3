822d8512c60be3b32d70ba90b538520362e5cbc9
cawfree <hello@cawfree.com>
2020-03-29 17:24:03 +0100

    Added webpack support.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ module.exports = {
         test: /\.mp3/i,
         use: 'file-loader',
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
     ],
   },
   optimization: {
