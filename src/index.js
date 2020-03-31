e207af187c5c3fbd1aeef16b41f2cbe1353dc57f
cawfree <hello@cawfree.com>
2020-03-31 19:53:06 +0100

    Moved world to class.

--- src/index.js
+++ src/index.js
@@ -5583,11 +5583,8 @@ class RocketLauncher extends Weapon {
   }
 }
 
-//////////////////////////////////////////////////////////////////////
-// World class - Helper for world chunks
-//////////////////////////////////////////////////////////////////////
-function World() {
-    // Generic chunk
+class World {
+  constructor() {
     this.obj_size_x = 16;
     this.obj_size_z = 16;
     this.obj_size_y = 2;
@@ -5601,8 +5598,8 @@ function World() {
     this.rpc_max = 0;
     this.obj_type = "world";
     this.base_type = "world";
-
-  World.prototype.reset = function(store) {
+  }
+  reset(store) {
     for (var i = 0; i < this.chunks.length; i++) {
       if(this.chunks[i].mesh) {
         store.scene.remove(this.chunks[i].mesh);
@@ -5614,14 +5611,12 @@ function World() {
     this.rebuild_idx = 0;
     this.rpc = 0;
     this.rpc_max = 0;
-  };
-
-    World.prototype.init = function(store) {
-        this.textures = new Textures();
-        this.textures.prepare();
-    };
-
-  World.prototype.removeBatch = function(store, points) {
+  }
+  init(store) {
+    this.textures = new Textures();
+    this.textures.prepare();
+  }
+  removeBatch(store, points) {
     for(var i = 0; i < points.length; i++) {
       var c = this.getChunkId(store, points[i].x, points[i].y, points[i].z, false);
       if(c.length == 0) { 
@@ -5631,205 +5626,198 @@ function World() {
         this.chunks[c[n]].rmBlock(store, points[i].x, points[i].y, points[i].z);
       }
     }
-  };
+  }
+  getChunkId(store, x, y, z, create) {
+    // ?
+    x |= 0;
+    y |= 0;
+    z |= 0;
 
-    World.prototype.getChunkId = function(store, x, y, z, create) {
-        x |= 0;
-        y |= 0;
-        z |= 0;
+    var finds = [];
+    var c = 0;
+    for (var i = 0; i < this.chunks.length; i++) {
+      // Split for perf.
+      if (x >= this.chunks[i].from_x && x <= this.chunks[i].to_x) {
+        if(z >= this.chunks[i].from_z && z <= this.chunks[i].to_z) {
+          if(y >= this.chunks[i].from_y && y <= this.chunks[i].to_y ) {
+            finds[c++] = i;
+          }
+        }
+      }
+    }
+    if(finds.length > 0) {
+      return finds;
+    }
+    if (create) {
+      // Create chunk based on world division by obj_size_x.
+      var pos_x = (x/this.obj_size_x)|0;
+      var pos_y = (y/this.obj_size_y)|0;
+      var pos_z = (z/this.obj_size_z)|0;
 
-        var finds = [];
-        var c = 0;
-        for (var i = 0; i < this.chunks.length; i++) {
-            // Split for perf.
-            if (x >= this.chunks[i].from_x && x <= this.chunks[i].to_x) {
-                if(z >= this.chunks[i].from_z && z <= this.chunks[i].to_z) {
-                    if(y >= this.chunks[i].from_y && y <= this.chunks[i].to_y ) {
-                        finds[c++] = i;
-                    }
-                }
+      var chunk = new Chunk(
+        pos_x * this.obj_size_x,
+        pos_y * this.obj_size_y,
+        pos_z * this.obj_size_z,
+        this.obj_size_x, this.obj_size_y, this.obj_size_z,
+        "CREATED", 1, "world");
+      chunk.init(store);
+      var i = this.addChunk(chunk);
+      return [i];
+    }
+    return [];
+  }
+  hit(store, dmg, dir, type, pos) {
+    this.explode(store, pos.x, pos.y, pos.z, dmg, type);
+  }
+  addChunk(chunk) {
+    this.chunks[this.cid] = chunk;
+    this.chunks[this.cid].owner = this;
+    this.cid++;
+    return this.cid-1;
+  }
+  explode(store, x, y, z, power, type) {
+
+    x |= 0;
+    y |= 0;
+    z |= 0;
+
+    var pow = power*power;
+
+    var list = [];
+    var vx = 0, vy = 0, vz = 0, val = 0, offset = 0;
+    for (var rx = x-power; rx <= x+power; rx++) {
+      vx = Math.pow((rx-x), 2); 
+      for (var rz = z-power; rz <= z+power; rz++) {
+        vz = Math.pow((rz-z),2)+vx; 
+        for (var ry = y-power; ry <= y+power; ry++) {
+          if (ry > 0) {
+            val = Math.pow((ry-y),2) + vz;
+            if (val <= pow) {
+              list.push({x: rx, y: ry, z: rz});
             }
+          }
         }
-        if(finds.length > 0) {
-            return finds;
+      }
+    }
+    if (type == "missile" || type == "grenade") {
+      var pos = 0;
+      var pxp = x+power*2;
+      var pxm = x-power*2;
+      var pzp = z+power*2;
+      var pzm = z-power*2;
+      for (var i = 0; i < store.cdList.length; i++) {
+        const { owner } = store.cdList[i];
+        if (owner) {
+          const { hit } = owner;
+          if (typeof hit === "function") {
+            pos = owner.chunk.mesh.position;
+            if (pos.x >= pxm && pos.x <= pxp && pos.z >= pzm && pos.z <= pzp) {
+              owner.hit(store, power, new THREE.Vector3(0,0,0), "missile", new THREE.Vector3(x,y,z));
+            }
+          }
         }
-       if (create) {
-           // Create chunk based on world division by obj_size_x.
-           var pos_x = (x/this.obj_size_x)|0;
-           var pos_y = (y/this.obj_size_y)|0;
-           var pos_z = (z/this.obj_size_z)|0;
+      }
+    } else {
+      store.sounds.PlaySound(store, "bullet_wall", new THREE.Vector3(x,y,z), 500);
+    }
+    this.removeBatch(store, list);
+  }
+  checkExists(store, pos) {
+    pos.x |= 0;
+    pos.y |= 0;
+    pos.z |= 0;
+    var c = this.getChunkId(store, pos.x, pos.y, pos.z, false);
+    if(c.length == 0) {
+      return [];
+    }
 
-           var chunk = new Chunk(
-               pos_x * this.obj_size_x,
-               pos_y * this.obj_size_y,
-               pos_z * this.obj_size_z,
-               this.obj_size_x, this.obj_size_y, this.obj_size_z,
-               "CREATED", 1, "world");
-           chunk.init(store);
-           var i = this.addChunk(chunk);
-           return [i];
-       }
-        return [];
-    };
-
-    World.prototype.hit = function(store, dmg, dir, type, pos) {
-        this.explode(store, pos.x, pos.y, pos.z, dmg, type);
-    };
-
-    World.prototype.addChunk = function(chunk) {
-        this.chunks[this.cid] = chunk;
-        this.chunks[this.cid].owner = this;
-        this.cid++;
-        return this.cid-1;
-    };
-
-    World.prototype.explode = function(store, x, y, z, power, type) {
-
-        x |= 0;
-        y |= 0;
-        z |= 0;
-
-        var pow = power*power;
-
-        var list = [];
-        var vx = 0, vy = 0, vz = 0, val = 0, offset = 0;
-        for (var rx = x-power; rx <= x+power; rx++) {
-          vx = Math.pow((rx-x), 2); 
-          for (var rz = z-power; rz <= z+power; rz++) {
-            vz = Math.pow((rz-z),2)+vx; 
-            for (var ry = y-power; ry <= y+power; ry++) {
-              if (ry > 0) {
-                val = Math.pow((ry-y),2) + vz;
-                if (val <= pow) {
-                  list.push({x: rx, y: ry, z: rz});
-                }
+    var list = [];
+    for(var i = 0; i < c.length; i++) {
+      var r = this.chunks[c[i]].checkExists(store, pos.x, pos.y, pos.z);
+      if(r != -1) {
+        list.push(r);
+      }
+    }
+    return list;
+  }
+  addColorBlock(store, x, y, z, r, g, b) {
+    x |= 0;
+    y |= 0;
+    z |= 0;
+    var c = this.getChunkId(store, x,y,z, true);
+    if(c.length != 0) {
+      for (var i = 0; i < c.length; i++) {
+        // Do not add blood to non-existing blocks.
+        if(this.chunks[c[i]].blockExists(x, y, z)) {
+          this.chunks[c[i]].addBlock(store, x, y, z, r, g, b);
+          if(r <= 50  && g >= 200 && b < 105 && b >= 50) {
+            for(var p = 0; p < this.radioactive_blocks.length; p++) {
+              if(this.radioactive_blocks[p].x == x &&
+                this.radioactive_blocks[p].y == y &&
+                this.radioactive_blocks[p].z == z)
+              {
+                return;
+              }
+            }
+            this.radioactive_blocks[this.rpc_max++] = [x,y,z];
+          } else {
+            for(var p = 0; p < this.radioactive_blocks.length; p++) {
+              if(this.radioactive_blocks[p].x == x &&
+                this.radioactive_blocks[p].y == y &&
+                this.radioactive_blocks[p].z == z) {
+                this.radioactive_blocks[p] = 0;
+                break;
               }
             }
           }
         }
-        if (type == "missile" || type == "grenade") {
-          var pos = 0;
-          var pxp = x+power*2;
-          var pxm = x-power*2;
-          var pzp = z+power*2;
-          var pzm = z-power*2;
-          for (var i = 0; i < store.cdList.length; i++) {
-            const { owner } = store.cdList[i];
-            if (owner) {
-              const { hit } = owner;
-              if (typeof hit === "function") {
-                pos = owner.chunk.mesh.position;
-                if (pos.x >= pxm && pos.x <= pxp && pos.z >= pzm && pos.z <= pzp) {
-                  owner.hit(store, power, new THREE.Vector3(0,0,0), "missile", new THREE.Vector3(x,y,z));
-                }
-              }
-            }
+      }
+    }
+  }
+  addBlock(store, x, y, z, r, g, b) {
+    x |= 0;
+    y |= 0;
+    z |= 0;
+    var c = this.getChunkId(store, x,y,z, true);
+    if(c.length != 0) {
+      for (var i = 0; i < c.length; i++) {
+        this.chunks[c[i]].addBlock(store, x, y, z, r, g, b);
+      }
+    }
+  }
+  update(store, time, delta) {
+    if(!store.player.chunk) {
+      return;
+    }
+
+    for(var i = 0; i < this.chunks.length; i++) {
+      if(this.chunks[i].dirty) {
+        var t1 = Date.now();
+        this.chunks[i].build(store);
+        if((Date.now() - t1) > 5) {
+          break;
+        }
+      }
+    }
+
+    if(this.radioactive_blocks.length > 0) {
+      var v = 0;
+      for(var i = 0; i < 10; i++) {
+        v = Math.random()*this.radioactive_blocks.length|0;
+        if(this.radioactive_blocks[v] != 0) {
+          if(this.checkExists(store, new THREE.Vector3(this.radioactive_blocks[v][0], this.radioactive_blocks[v][1], this.radioactive_blocks[v][2])).length == 0) {
+            this.radioactive_blocks[v] = 0;
+          } else {
+            store.particles.radiation(
+              this.radioactive_blocks[v][0]+(1-Math.random()*2),
+              this.radioactive_blocks[v][1]+(1-Math.random()*2),
+              this.radioactive_blocks[v][2]+(1-Math.random()*2),
+            );
           }
-        } else {
-          store.sounds.PlaySound(store, "bullet_wall", new THREE.Vector3(x,y,z), 500);
         }
-      this.removeBatch(store, list);
-    };
-
-    World.prototype.checkExists = function(store, pos) {
-        pos.x |= 0;
-        pos.y |= 0;
-        pos.z |= 0;
-        var c = this.getChunkId(store, pos.x, pos.y, pos.z, false);
-        if(c.length == 0) {
-            return [];
-        }
-        
-        var list = [];
-        for(var i = 0; i < c.length; i++) {
-            var r = this.chunks[c[i]].checkExists(store, pos.x, pos.y, pos.z);
-            if(r != -1) {
-                list.push(r);
-            }
-        }
-        return list;
-    };
-
-    World.prototype.addColorBlock = function(store, x, y, z, r, g, b) {
-        x |= 0;
-        y |= 0;
-        z |= 0;
-        var c = this.getChunkId(store, x,y,z, true);
-        if(c.length != 0) {
-            for (var i = 0; i < c.length; i++) {
-                // Do not add blood to non-existing blocks.
-                if(this.chunks[c[i]].blockExists(x, y, z)) {
-                    this.chunks[c[i]].addBlock(store, x, y, z, r, g, b);
-                    if(r <= 50  && g >= 200 && b < 105 && b >= 50) {
-                        for(var p = 0; p < this.radioactive_blocks.length; p++) {
-                            if(this.radioactive_blocks[p].x == x &&
-                               this.radioactive_blocks[p].y == y &&
-                               this.radioactive_blocks[p].z == z)
-                            {
-                                return;
-                            }
-                        }
-                        this.radioactive_blocks[this.rpc_max++] = [x,y,z];
-                    } else {
-                        for(var p = 0; p < this.radioactive_blocks.length; p++) {
-                            if(this.radioactive_blocks[p].x == x &&
-                               this.radioactive_blocks[p].y == y &&
-                               this.radioactive_blocks[p].z == z) {
-                                this.radioactive_blocks[p] = 0;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    };
-
-    World.prototype.addBlock = function(store, x, y, z, r, g, b) {
-        x |= 0;
-        y |= 0;
-        z |= 0;
-        var c = this.getChunkId(store, x,y,z, true);
-        if(c.length != 0) {
-            for (var i = 0; i < c.length; i++) {
-                this.chunks[c[i]].addBlock(store, x, y, z, r, g, b);
-            }
-        }
-    };
-
-    World.prototype.update = function(store, time, delta) {
-        if(!store.player.chunk) {
-            return;
-        }
-
-        for(var i = 0; i < this.chunks.length; i++) {
-           if(this.chunks[i].dirty) {
-               var t1 = Date.now();
-               this.chunks[i].build(store);
-               if((Date.now() - t1) > 5) {
-                   break;
-               }
-            }
-        }
-
-        if(this.radioactive_blocks.length > 0) {
-            var v = 0;
-            for(var i = 0; i < 10; i++) {
-                v = Math.random()*this.radioactive_blocks.length|0;
-                if(this.radioactive_blocks[v] != 0) {
-                    if(this.checkExists(store, new THREE.Vector3(this.radioactive_blocks[v][0], this.radioactive_blocks[v][1], this.radioactive_blocks[v][2])).length == 0) {
-                        this.radioactive_blocks[v] = 0;
-                    } else {
-                        store.particles.radiation(
-                         this.radioactive_blocks[v][0]+(1-Math.random()*2),
-                         this.radioactive_blocks[v][1]+(1-Math.random()*2),
-                         this.radioactive_blocks[v][2]+(1-Math.random()*2),
-                        );
-                    }
-                }
-            }
-        }
-    };
+      }
+    }
+  }
 }
 
 new Main().init();
